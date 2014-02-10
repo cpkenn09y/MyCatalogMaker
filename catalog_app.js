cb43cde82ef6ea6d80d734ac9254bfc29c4633bf
Kenneth Yu <kenyu@ucdavis.edu>
2014-02-09 20:50:06 -0800

    clear the form fields after submission

--- catalog_app.js
+++ catalog_app.js
@@ -17,12 +17,19 @@ CatalogApp.prototype.enableItemCreation = function() {
 
 CatalogApp.prototype.createItem = function(event) {
   event.preventDefault()
-  var name = this["name"].value
-  var price = this["price"].value
+  submittedForm = this
+  var name = submittedForm["name"].value
+  var price = submittedForm["price"].value
   var item = new Item({name: name, price: price}).save()
+  self.clearFormFields(submittedForm)
+}
+
+CatalogApp.prototype.clearFormFields = function(form) {
+  form.reset()
 }
 
 $(document).ready(function() {
   var myCatalogApp = new CatalogApp()
+  catalog = new Catalog()
   myCatalogApp.initialize()
 })
