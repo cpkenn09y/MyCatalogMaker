adf41f054dbd77eb14b4ff3c0ddaddd6548d1648
Kenneth Yu <kenyu@ucdavis.edu>
2014-02-10 03:27:57 -0800

    added table headers

--- views/catalog_view.js
+++ views/catalog_view.js
@@ -13,6 +13,7 @@ var CatalogView = Backbone.View.extend({
   render : function(){
     this.$el.empty()
     this.$el.append('<table class="item-list"></table>')
+    this.$el.append('<tr><th>ITEM NAME</th><th>DESCRIPTION</th><th>WIDTH</th><th>LENGTH</th><th>HEIGHT</th><th>WEIGHT</th><th>PRICE</th></tr>')
     this.iterateCollection(this.collection)
   }
 })
